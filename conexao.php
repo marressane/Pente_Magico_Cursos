c7b8f36d3f29a6964d7ee040dd3382eb4ffcc65c
JMulungo-xvii <jacintoenigmamulungo@gmail.com>
2021-07-04 21:30:30 +0200

    conexao

--- conexao.php
+++ conexao.php
@@ -11,7 +11,7 @@ mysqli_set_charset ($con, '');
 //check Connection
 if(mysqli_connect_errno())
  {
-echo "Failed to connect to DB: " . mysqli_connect_errno();
+echo "Failed to connect to DB: " . mysqli_connect_error();
 
  }
 
